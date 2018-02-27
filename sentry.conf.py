abd32b2045714c3944ea17124c3d263c61a22275
Daniel Barreto <daniel.barreto.n@gmail.com>
2018-02-27 21:34:44 +0100

    Fix `SENTRY_EMAIL_HOST` configuration

--- sentry.conf.py
+++ sentry.conf.py
@@ -255,7 +255,7 @@ SENTRY_WEB_OPTIONS = {
 email = env('SENTRY_EMAIL_HOST') or (env('SMTP_PORT_25_TCP_ADDR') and 'smtp')
 if email:
     SENTRY_OPTIONS['mail.backend'] = 'smtp'
-    SENTRY_OPTIONS['mail.host'] = email
+    SENTRY_OPTIONS['mail.host'] = env('SENTRY_EMAIL_HOST')
     SENTRY_OPTIONS['mail.password'] = env('SENTRY_EMAIL_PASSWORD') or ''
     SENTRY_OPTIONS['mail.username'] = env('SENTRY_EMAIL_USER') or ''
     SENTRY_OPTIONS['mail.port'] = int(env('SENTRY_EMAIL_PORT') or 25)
